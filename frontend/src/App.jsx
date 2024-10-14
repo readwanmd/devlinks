707e79c9d35dcf05539d3e0c99d85b8275bbd7ea
readwanmd <readwanmd@gmail.com>
2024-10-14 21:05:33 +0600

    netlify redirect added, global toast for onrender meta teg updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from './routes/AppRoutes';
 
 const App = () => {
+	useEffect(() => {
+		toast(
+			'Backend is hosted at onrender, It take around 50 seconds to start the server. please consider this time.',
+			{
+				autoClose: 8000,
+				hideProgressBar: false,
+				position: 'top-center',
+			}
+		);
+	}, []);
+
 	return (
 		<>
 			<Router>
